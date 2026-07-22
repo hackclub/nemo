0a1b7362d9309dc8381e937b82dd1967bec1025f
Abdallah Ebrahim (dracula) <abdallah.ebrahim.official@gmail.com>
2026-07-22 13:25:22 +0300

    web: cohort selector for onboarding funnel

--- web/app/controllers/home_controller.rb
+++ web/app/controllers/home_controller.rb
@@ -9,7 +9,9 @@ class HomeController < ApplicationController
     @top_channels = Analytics::MartChannelActivity.order(messages_posted: :desc).limit(8)
     @activity_bands = Analytics::MartActivityDistribution.order(:band_order)
     @account_types = Analytics::MartAccountType.order(members: :desc)
-    @onboarding_funnel = Analytics::MartOnboardingFunnel.order(cohort_month: :desc).first
+    @cohort_months = Analytics::MartOnboardingFunnel.order(cohort_month: :desc).pluck(:cohort_month)
+    selected_cohort = params[:cohort_month].presence&.then { |d| Date.parse(d) } || @cohort_months.first
+    @onboarding_funnel = Analytics::MartOnboardingFunnel.find_by(cohort_month: selected_cohort)
     @channel_scorecard = Analytics::MartChannelOnboardingScorecard.order(post_month: :desc, newcomer_volume: :desc).limit(10)
     @fast_reply_vs_retention = Analytics::MartFastReplyVsRetention.order(fast_reply: :desc)
     @recurrence_funnel = Analytics::MartOnboardingRecurrenceFunnel.take
