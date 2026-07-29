74caa32aca91b5232f9d2cf5a8f5a0fea90ba474
Abdallah Ebrahim (dracula) <abdallah.ebrahim.official@gmail.com>
2026-07-29 21:12:43 +0300

    web: keep the activity granularity across a refresh

--- web/app/javascript/controllers/activity_granularity_controller.js
+++ web/app/javascript/controllers/activity_granularity_controller.js
@@ -5,10 +5,13 @@ export default class extends Controller {
   static values = { url: String }
 
   async change() {
-    const params = new URLSearchParams({ activity_granularity: this.selectTarget.value })
-    const resp = await fetch(`${this.urlValue}?${params}`, {
+    const url = new URL(window.location)
+    url.searchParams.set("activity_granularity", this.selectTarget.value)
+
+    const resp = await fetch(`${this.urlValue}?${url.searchParams}`, {
       headers: { "X-Requested-With": "activity-charts", Accept: "text/html" }
     })
     this.containerTarget.innerHTML = await resp.text()
+    history.replaceState({}, "", url)
   }
 }
