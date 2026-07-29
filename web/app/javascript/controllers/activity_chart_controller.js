d12cbd05254b829714ba1613a80b0efcaa048f0c
Abdallah Ebrahim (dracula) <abdallah.ebrahim.official@gmail.com>
2026-07-29 18:34:15 +0300

    web: put activity chart ticks on fixed 15-day intervals

--- web/app/javascript/controllers/activity_chart_controller.js
+++ web/app/javascript/controllers/activity_chart_controller.js
@@ -13,7 +13,11 @@ import {
 Chart.register(LineController, LineElement, PointElement, CategoryScale, LinearScale, Tooltip, Legend)
 
 export default class extends Controller {
-  static values = { data: Object }
+  static values = { data: Object, tickEvery: Number }
+
+  get tickEvery() {
+    return this.hasTickEveryValue && this.tickEveryValue > 0 ? this.tickEveryValue : 15
+  }
 
   connect() {
     this.chart = new Chart(this.element, {
@@ -25,7 +29,17 @@ export default class extends Controller {
         elements: { point: { radius: 0, hitRadius: 8 }, line: { borderWidth: 2, tension: 0.2 } },
         plugins: { legend: { display: true } },
         scales: {
-          x: { ticks: { maxTicksLimit: 8, autoSkip: true } },
+          x: {
+            ticks: {
+              autoSkip: false,
+              maxRotation: 0,
+              callback: (value, index) => {
+                const labels = this.dataValue.labels
+                if (index === labels.length - 1) return labels[index]
+                return index % this.tickEvery === 0 ? labels[index] : null
+              },
+            },
+          },
           y: { beginAtZero: true },
         },
       },
