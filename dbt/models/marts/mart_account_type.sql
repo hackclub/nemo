9d0d236a1c752e5b4c3cb5aaa0829ecdd5f99097
Abdallah Ebrahim (dracula) <abdallah.ebrahim.official@gmail.com>
2026-08-03 20:31:21 +0300

    dbt: drop the messages column from account composition

--- dbt/models/marts/mart_account_type.sql
+++ dbt/models/marts/mart_account_type.sql
@@ -1,19 +1,9 @@
-with member_totals as (
-    select
-        user_id,
-        sum(coalesce(messages_posted, 0)) as messages_posted
-    from {{ ref('fct_member_activity') }}
-    group by user_id
-)
-
 select
     m.account_type,
     count(*) as members,
     count(*) filter (where m.is_claimed) as claimed,
-    sum(coalesce(act.messages_posted, 0)) as total_messages,
-    'v8' as metric_version
+    'v9' as metric_version
 from {{ ref('dim_member') }} m
-left join member_totals act using (user_id)
 where m.is_live
 group by 1
 order by members desc
