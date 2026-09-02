30ea5647f8d0d5efbe2f99771eb7e86faf2f4504
Abdallah Ebrahim (dracula) <abdallah.ebrahim.official@gmail.com>
2026-09-02 16:50:21 +0300

    web: give mnemosyne it's own identity

--- web/config/routes.rb
+++ web/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
+  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   get "login", to: "sessions#new", as: :login
   match "auth/:provider/callback", to: "sessions#create", via: [:get, :post], as: :auth_callback
