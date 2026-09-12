80d90f6f7d9a24b63e8922ac3679c84dad36e1d1
dracula <abdallah.ebrahim.official@gmail.com>
2026-09-12 14:59:34 +0300

    web: let a blank in deploy/.env mean unset

--- web/config/boot.rb
+++ web/config/boot.rb
@@ -5,7 +5,11 @@ require "bundler/setup" # Set up gems listed in the Gemfile.
 begin
   require "dotenv"
   env_file = File.expand_path("../../deploy/.env", __dir__)
-  Dotenv.load(env_file) if File.exist?(env_file)
+  if File.exist?(env_file)
+    Dotenv.parse(env_file).each do |name, value|
+      ENV[name] = value unless value.to_s.empty? || ENV.key?(name)
+    end
+  end
 rescue LoadError
   nil
 end
